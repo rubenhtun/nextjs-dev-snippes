93adb4874c3b803686950911ba51bc59923a85b2
Ruben Htun <rubenhtun2003@gmail.com>
2025-01-22 15:08:49 +0700

    index.html

--- jwt-login/frontend/src/PrivateRoutes.tsx
+++
@@ -1,7 +0,0 @@
-import { Outlet } from "react-router-dom";
-import Login from "./components/Login";
-
-export function PrivateRoutes() {
-  const token = localStorage.getItem("token");
-  return <>{token ? <Outlet /> : <Login />}</>;
-}
